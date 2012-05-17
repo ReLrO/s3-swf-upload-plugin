a5cc400a2a6a5fbc374c0d70426666b37a6c99d6
Ariel Rosenthal <relrosen@hotmail.com>
2012-05-17 11:04:57 +0300

    changed s3 yaml file path

--- lib/s3_swf_upload/railties/generators/uploader/uploader_generator.rb
+++ lib/s3_swf_upload/railties/generators/uploader/uploader_generator.rb
@@ -5,7 +5,7 @@ module S3SwfUpload
     class UploaderGenerator < Base
       
       def create_uploader
-        copy_file 'amazon_s3.yml', File.join('config','amazon_s3.yml')
+        copy_file 'amazon_s3.yml', File.join('config','s3.yml')
         copy_file 's3_uploads_controller.rb', File.join('app','controllers', 's3_uploads_controller.rb')
         copy_file 's3_upload.js', File.join('public','javascripts', 's3_upload.js')
         copy_file 's3_upload.swf', File.join('public','flash', 's3_upload.swf')
